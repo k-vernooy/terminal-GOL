7bbcc112c53d9814e95164cde7c47516b9cc0d94
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-01 23:16:29 -0400

    Basic start screen

---
+++ game.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+sleep .2
+
+echo "Welcome to the terminal recreation of Conway's
+  ____    _    __  __ _____    ___  _____   _     ___ _____ _____ 
+ / ___|  / \  |  \/  | ____|  / _ \|  ___| | |   |_ _|  ___| ____|
+| |  _  / _ \ | |\/| |  _|   | | | | |_    | |    | || |_  |  _|  
+| |_| |/ ___ \| |  | | |___  | |_| |  _|   | |___ | ||  _| | |___ 
+ \____/_/   \_\_|  |_|_____|  \___/|_|     |_____|___|_|   |_____|"
+
+sleep 2
+echo -e "\nPress any key to continue."
+
+clear
+
+echo "Move cursor with the error keys.
+'i' = place block.
+'x' = remove block 
+'s' = begin simulation"
+
+sleep 1
