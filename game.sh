5b55292af9c037ec456373aeb4e4b3066017234e
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-03 23:52:22 -0400

    Added error noises

--- game.sh
+++ game.sh
@@ -29,42 +29,51 @@ coordx=0
 coordy=0
 
 while true; do
-    read -r -sn1 t
-    case $t in
+    read -r -sn3 t
+    case "${t:2:1}" in
         A) 
             
             if [ $coordy -ne 0 ]; then
                 ((coordy--))
+            else
+                tput bel
             fi
             tput cup $coordy $coordx
-            
+
         ;;
         B) 
             lines=$(tput lines | tr -d ' ') 
             if [ $coordy -ne $lines ]; then
                 ((coordy++)) 
+            else
+                tput bel
             fi
             tput cup $coordy $coordx
+
         ;;
         C)
             cols=$(tput cols | tr -d ' ') 
             if [ $coordx -ne $cols ]; then
                 ((coordx++))
+            else
+                tput bel
             fi
             tput cup $coordy $coordx
-             
+
         ;;
         D) 
             
             if [ $coordx -ne 0 ]; then
                 ((coordx--))
+            else 
+                tput bel
             fi
             tput cup $coordy $coordx
-            
+
         ;;
         s)
             printf "gots to start!!!"
-            sleep 1
+
             break
         ;;
         i)
