dae0bda574b27d55bb5419c715a7a0ff15bcccb3
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-05 14:49:21 -0400

    Added neighbors function

--- iterCalc.cpp
+++ iterCalc.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <vector> 
+#include <sstream> 
+// #include <string>
 
 using namespace std;
 
+void neighbors(std::string data) {
+    // string n1, n2, n3;
+    std::string d1 = data.substr(0, 1); 
+    std::string d2 = data.substr(2, 1); 
+
+    stringstream data1(d1); 
+    int c1 = 0; 
+    data1 >> c1; 
+
+    stringstream data2(d2); 
+    int c2 = 0; 
+    data2 >> c2; 
+
+    cout << c1 << ", " << c2 << endl;
+}
+
 void relevantTiles(vector<string> input) {
     for ( int i = 0; i < input.size(); i++) {
-        cout << input[i] << endl;
+        // cout << input[i] << endl;
+        neighbors(input[i]);
     }
 }
 
