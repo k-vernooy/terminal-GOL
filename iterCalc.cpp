de052d276aae6aa4b8047935c8e23ecc5ba86617
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-04 22:48:20 -0400

    Add and print out a vector of data

--- iterCalc.cpp
+++ iterCalc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector> 
 
 using namespace std;
 
@@ -6,8 +7,12 @@ int main(int argc, char *argv[]) {
     std::cout << "height: " << argv[1] << endl;
     std::cout << "width: " << argv[2] << endl;
 
+    vector<string> data;
+
     for ( int i = argc - 1; i > 2; i--) {
-        std::cout << "active: " << argv[i] << endl;
-        // std::cout << i << endl;
+        // std::cout << "active: " << argv[i] << endl;
+        data.push_back(argv[i]);
     }
+    for(int i=0; i < data.size(); ++i)
+    std::cout << data[i] << ' ';
 }
