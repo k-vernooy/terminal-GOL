cb4866c57caed37df199a8bb5038b0fe9cf4be87
k-vernooy <kai.a.vernooy@gmail.com>
2019-08-06 13:29:10 -0400

    FIxed bug with single digit splitting

--- iterCalc.cpp
+++ iterCalc.cpp
@@ -8,16 +8,29 @@ using namespace std;
 
 vector<string> neighbors(string data) {
     string n1, n2, n3, n4, n5, n6, n7, n8;
-    string d1 = data.substr(0, 1); 
-    string d2 = data.substr(2, 1); 
 
-    stringstream data1(d1); 
-    int c1 = 0; 
-    data1 >> c1; 
+    replace(data.begin(), data.end(), '_', ' ');
 
-    stringstream data2(d2); 
-    int c2 = 0; 
-    data2 >> c2; 
+    vector<int> array;
+    stringstream ss(data);
+    int temp;
+    while (ss >> temp)
+    array.push_back(temp);
+
+    // string d1 = data.substr(0, 1); 
+    // string d2 = data.substr(2, 1); 
+
+    // NEED TO CORRECTLY REBUILD D1 AND D2
+
+    // stringstream data1(d1); 
+    // int c1 = 0; 
+    // data1 >> c1; 
+
+    // stringstream data2(d2); 
+    // int c2 = 0; 
+    // data2 >> c2; 
+    int c1 = array[0];
+    int c2 = array[1];
 
     int c1p, c1m, c2p, c2m;
     c1p = c1 + 1;
@@ -45,12 +58,9 @@ vector<int> liveNeighbors(vector<string> live, vector<string> tiles) {
 
     for ( int i = 0; i < tiles.size(); i++) {
         vector<string> neighborsList = {};
-        // cout << "getting neighbors of " << tiles[i] << endl;
         neighborsList = neighbors(tiles[i]);
 
-        // for (int f = 0; f < 8; f++) {
-        //     cout << "\t" << neighborsList[i] << endl;
-        // }
+
         // for each neighbor of every tile:
         int liveNeCount = 0;
         for ( int j = 0; j < 8; j++) {
@@ -91,18 +101,13 @@ vector<string> relevantTiles(vector<string> input) {
         relevantiles.push_back(input[i]);
     }
 
-    // Relevantiles array has all of input stored in it
+    // Relevantiles array has all live tiles stored in it
 
     // for each live tile
     for ( int i = 0; i < input.size(); i++) {
-
         vector<string> neighborArray = neighbors(input[i]);
-
         for ( int x = 0; x < 8; x++) {
             string curTile = neighborArray[x];
-
-            //Checking if v contains the element x:
-
             if ( find(relevantiles.begin(), relevantiles.end(), curTile) != relevantiles.end() )
                 cout << "";
             else
@@ -114,10 +119,8 @@ vector<string> relevantTiles(vector<string> input) {
 
 int main(int argc, char *argv[]) {
     // int height, width;
-
     // height = argv[1];
     // width = argv[2];
-    
     // cout << "height: " << argv[1] << endl;
     // cout << "width: " << argv[2] << endl;
 
@@ -130,6 +133,11 @@ int main(int argc, char *argv[]) {
     
     vector<string> tiles = relevantTiles(data);
 
+    // for ( int i = 0; i < 8; i++ ) {
+    //     cout << neighbors("7_9")[i] << endl;
+    // }
+    
+
     vector<bool> live;
 
     for ( int i = 0; i < tiles.size(); i++) {
@@ -139,7 +147,6 @@ int main(int argc, char *argv[]) {
         else {
             live.push_back(false);
         }
-
     }
 
     vector<int> neighbors = liveNeighbors(data, tiles);
